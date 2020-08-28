9b99f318914d151da4354e2494e62ac304f6d012
seanmurphy611 <spm6407@psu.edu>
2020-08-28 20:11:33 +0000

    period

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ name = input("Enter your name: ")
 print("Hello, " + name +"!")
 age = input("Enter your age: ")
 age = int(age)
-print(f"You will be {age+1} next year")
+print(f"You will be {age+1} next year.")
 
